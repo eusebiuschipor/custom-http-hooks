bf081cb82a62b1413770c06a48cd9e2e4e5ad9fd
eusebiuschipor <eusebiu@eusebiuschipor.com>
2021-05-29 00:53:06 +0300

    changed task form

--- src/components/NewTask/TaskForm.js
+++ src/components/NewTask/TaskForm.js
@@ -1,23 +1,21 @@
-import { useRef } from 'react';
+import { useState } from 'react';
 
 import './TaskForm.css';
 
 const TaskForm = (props) => {
-  const taskInputRef = useRef();
+  const [text, setText] = useState('');
 
   const submitHandler = (event) => {
     event.preventDefault();
 
-    const enteredValue = taskInputRef.current.value;
-
-    if (enteredValue.trim().length > 0) {
-      props.onEnterTask(enteredValue);
+    if (text.trim().length > 0) {
+      props.onEnterTask(text);
     }
   };
 
   return (
     <form className="form" onSubmit={submitHandler}>
-      <input type='text' ref={taskInputRef} />
+      <input type='text' value={text} onChange={(e) => setText(e.target.value)} />
       <button>{props.loading ? 'Sending...' : 'Add Task'}</button>
     </form>
   );
